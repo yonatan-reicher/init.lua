33e03f0b4f50710923fa9e71719bd096b2beb0b4
Yonatan Reicher <yony252525@gmail.com>
2025-02-23 19:41:43 +0200

    Set scrolloffset = 2

--- lua/options.lua
+++ lua/options.lua
@@ -7,6 +7,9 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
+-- Keep 2 lines above and below the cursor at all times.
+vim.opt.scrolloff = 2
+
 -- Detect filetype.
 vim.cmd("filetype on")
 -- Add koka filetype.
