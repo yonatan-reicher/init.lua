8614f9b41fe329758daae0d2adf35de961ab4043
Yonatan Reicher <yony252525@gmail.com>
2025-07-16 19:57:18 +0300

    Colorscheme update

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,3 +1,4 @@
+util = require("tokyonight.util")
 require("tokyonight").setup({
     style = "night",
     styles = {
@@ -7,21 +8,29 @@ require("tokyonight").setup({
       variables = {},
     },
     day_brightness = 0.25, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-    hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-    dim_inactive = false, -- dims inactive windows
+    hide_inactive_statusline = true,
+    dim_inactive = true, -- dims inactive windows
+    terminal_colors = true, -- whether to set the terminal colors (e.g. `:terminal`, `:!`, etc.)
 
     --- You can override specific color groups to use other groups or a hex color
     --- function will be called with a ColorScheme table
     ---@param colors ColorScheme
     on_colors = function(colors) 
+        -- print(vim.inspect(colors))
         if vim.o.background == "dark" then
-            colors.bg = "#1e1e1e"
+            colors.gray = "#2d2d30"
+            colors.bg = "#1c1c1c"
+            colors.bg_dark = util.blend(colors.bg, 0.35, colors.black)
             colors.comment = "#6d7481"
             colors.green1 = "#4ec9b0"
         else
-            colors.bg = "#ffffff"
-            colors.green1 = "#4ec9b0"
+            colors.gray = "#dddded"
+            colors.bg = "#fdfdfd"
+            colors.fg = "#101030"
+            colors.green1 = "#43b07d"
+            colors.bg_dark = util.blend(colors.bg, 0.5, colors.gray)
         end
+        colors.dark_gray = util.blend(colors.gray, 0.5, colors.black)
     end,
     
     --- You can override specific highlights to use other groups or a hex color
@@ -33,10 +42,22 @@ require("tokyonight").setup({
         highlights['@keyword'] = { fg = colors.purple, italic = false }
         highlights['@property'] = '@variable'
         highlights['@variable.member'] = '@variable'
+        highlights['@variable.builtin'] = 'Special'
         highlights.Type.fg = colors.green1
         highlights.Constant.fg = colors.blue1
         highlights.TabLine.fg = colors.blue
         highlights.Conditional = '@keyword'
+        highlights.ColorColumn.bg = colors.bg_dark1
+        highlights.Directory.fg = colors.blue1
+        highlights.WinSeparator.fg = colors.blue
+        highlights.SignColumn.bg = nil
+        highlights.LineNr.fg = highlights.CursorLineNr.fg
+        highlights.MatchParen.bg = colors.black
+        highlights.MatchParen.italic = true
+        highlights.StatusLine.bg = colors.gray
+        highlights.StatusLineNC.bg = colors.dark_gray
+        highlights['@comment.todo'] = highlights.Todo
+        highlights.Special.fg = nil
     end,
 
     -- All on_colors and on_highlights table keys and default values are here:
