90b01434286a84e5acf7de3c52a799170deab4cd
Jonathan Reicher <yony252525@gmail.com>
2025-07-16 20:29:18 +0300

    Comment

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 local path_to_this = vim.env.myvimrc
 local this_dir = vim.fs.dirname(path_to_this)
+-- This tells Neovim to load plugins and packages from this directory
 vim.opt.packpath:append { this_dir }
 
 require('plugins')
