663a0a43e9d03140f792fa2b43567f6d4006a35f
Jonathan Reicher <yony252525@gmail.com>
2025-07-16 20:28:29 +0300

    No more removing shared data files

--- init.lua
+++ init.lua
@@ -12,12 +12,12 @@ if vim.fn.executable('nu') == 1 then
     require('nu')
 end
 
--- We need to remove this useless auto-generated file
-data_path = vim.fn.stdpath('data')
-filname_to_remove = data_path .. "\\shada\\main.shada.tmp.X"
-if vim.fn.filereadable(filname_to_remove) == 1 then
-    vim.fn.delete(filname_to_remove)
-end
+-- -- We need to remove this useless auto-generated file
+-- data_path = vim.fn.stdpath('data')
+-- filname_to_remove = data_path .. "\\shada\\main.shada.tmp.X"
+-- if vim.fn.filereadable(filname_to_remove) == 1 then
+--     vim.fn.delete(filname_to_remove)
+-- end
 
 -- TODO:
 -- - Look into :set exrc
