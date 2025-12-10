470e33c9510128126bb5883fc6164ccf5cf5c836
Jonathan Reicher <yony252525@gmail.com>
2025-12-10 19:51:34 +0200

    update options

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,6 @@
-vim.opt.nu = true
-vim.opt.rnu = true
+-- Line numbers and relative line numbers.
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- Tab and white-space settings.
 vim.opt.tabstop = 4
@@ -58,68 +59,37 @@ vim.opt.spell = true
 -- Persistent undo.
 vim.opt.undofile = true
 
+-- Clipboard
+vim.opt.clipboard:append 'unnamedplus'
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank {
+        higroup = 'Highlight',
+        timeout = 400,
+        on_macro = true,
+        on_visual = true,
+    }
+  end,
+})
+
+vim.g.have_nerd_font = true
 
 
--- These are settings I am interested in checking out
---[==[
-vim.cmd[[colorscheme habamax]]
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.g.have_nerd_font = true
 vim.opt.list = true
 vim.opt.listchars = { tab = '> ', trail = '·' }
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.inccommand = 'split'
 vim.opt.breakindent = true
-vim.opt.undofile = true
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.winborder = 'rounded'
-vim.opt.winborder = 'shadow'
-vim.opt.clipboard:append('unnamedplus')
-vim.colorcolumn = 80
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>', { desc = "Exit terminal on window operations" })
-
--- Tab options
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-
-
--- Lsp
-vim.lsp.config['rust-analyzer'] = {
-    cmd = { 'rust-analyzer' },
-    filetypes = { 'rust' },
-    root_markers = { { 'Cargo.toml', 'Cargo.lock' }, '.git' }
-}
-vim.lsp.enable('rust-analyzer')
-
---]==]
-
-
---[==[
--- Some more things I took from somewhere, should consider what is interesting
--- here
--- Custom Options
-vim.o.clipboard = 'unnamedplus'
 
 local opts = { noremap = true, silent = true }
-
--- Remap esc to jk so that you can exit insert mode by jk fast sequence
-vim.keymap.set('i', 'jk', '<Esc>', opts)
-
--- Vertical scroll and center
-vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
-vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
 
 -- Find and center
 vim.keymap.set('n', 'n', 'nzzzv', opts)
@@ -129,57 +99,13 @@ vim.keymap.set('n', 'N', 'Nzzzv', opts)
 vim.keymap.set('v', '<', '<gv', opts)
 vim.keymap.set('v', '>', '>gv', opts)
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
 -- set line wrap
 vim.keymap.set('n', '<leader>lw', ':set wrap! <CR>', { desc = 'Toggle line wrap' })
-
--- Save files
-vim.keymap.set('n', '<leader>ff', ':wall <CR>', opts)
-
--- Buffers
-vim.keymap.set('n', '<leader>bd', ':bd<CR>', { desc = 'Delete the current buffer.' })
-vim.keymap.set('n', '<leader>bda', ':%bd|edit#|bd#<CR>', {desc = "Delete all buffers except the current buffer."})
 
 -- Cycle through the buffers
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
 vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
 
--- Setup for tabs spacing
-vim.opt.tabstop = 3 -- Number of spaces a <Tab> counts for
-vim.opt.shiftwidth = 3 -- Number of spaces to use for each step of (auto)indent
-vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.softtabstop = 3 -- Number of spaces for <Tab> in insert mode
-
--- Resize windows with <leader>w[
+-- Resize windows with <leader>w[/]
 vim.keymap.set('n', '<leader>w[', function() vim.cmd('vertical resize -5') end, { desc = 'Shrink window width' })
-
--- Resize windows with <leader>w]
 vim.keymap.set('n', '<leader>w]', function() vim.cmd('vertical resize +5') end, { desc = 'Increase window width' })
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
-
--- Toggle autosave. You have to install this plugin as I described above.
-vim.keymap.set('n', '<leader>ts', ':ASToggle<CR>', { desc = 'Toggle auto save' })
-
--- Show relative line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
-
--- Open file tree. See the file tree section to enable this plugin 
-vim.keymap.set('n', '<leader>ft', ':Neotree current <CR>', { desc = 'Open file tree' })
-
--- Split window
-vim.keymap.set('n', '<leader>wv', '<C-w>v', { desc = 'Split window vertically' })
-vim.keymap.set('n', '<leader>wcs', '<C-w>s', { desc = 'Split window horizontally' })
-
--- Navigate between windows here <CR> simulates the enter press
-vim.keymap.set('n', '<leader>wh', ':wincmd h<CR>', { desc = 'Navigate to left split' })
-vim.keymap.set('n', '<leader>wl', ':wincmd l<CR>', { desc = 'Navigate to right split' })
-vim.keymap.set('n', '<leader>wk', ':wincmd k<CR>', { desc = 'Navigate to upper split' })
-vim.keymap.set('n', '<leader>wj', ':wincmd j<CR>', { desc = 'Navigate to lower split' })
-
-
---]==]
