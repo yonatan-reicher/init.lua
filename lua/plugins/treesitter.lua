c9a54cee38503dcc2a7466760ffc3b3105fafdd8
Jonathan Reicher <yony252525@gmail.com>
2026-03-28 08:24:22 +0300

    treesitter sync install (makes it easier to see problems)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -16,7 +16,7 @@ if moduleExists 'nvim-treesitter' then
         },
     
         -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
+        sync_install = true,
     
         -- Automatically install missing parsers when entering buffer
         auto_install = false,
