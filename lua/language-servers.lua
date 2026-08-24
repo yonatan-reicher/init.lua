79883ee9bcbb7188e0f33eb51c6eaec05b96f632
Jonathan Reicher <yony252525@gmail.com>
2026-08-24 11:38:13 +0300

    update lua/language-servers.lua

--- lua/language-servers.lua
+++ lua/language-servers.lua
@@ -1,5 +1,16 @@
+vim.lsp.config('*', {
+    root_markers = { '.git' },
+})
 vim.lsp.config('clangd', {
-    cmd = { "clangd", "--fallback-style=Microsoft" },
+    cmd = {
+        'clangd',
+        '--fallback-style=Microsoft',
+        '--background-index',
+        '--clang-tidy',
+        '--offset-encoding=utf-8',
+    },
+    root_markers = { '.clangd', 'compile_commands.json' },
+    filetypes = { 'c', 'cpp' },
 })
 vim.lsp.config('rust_analyzer', {
     settings = {
@@ -19,6 +30,12 @@ vim.lsp.enable {
     'dafny',
     'elmls',
     'fsautocomplete',
-    'pyright',
     'rust_analyzer',
 }
+local python_lsps = { 'zuban', 'pyright', 'ty' }
+for _, lsp in ipairs(python_lsps) do
+    if vim.fn.executable(lsp) then
+        vim.lsp.enable(lsp)
+        break
+    end
+end
