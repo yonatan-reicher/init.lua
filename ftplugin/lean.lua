45d672daebf4fbf89331741688b7a410ab5cf608
Jonathan Reicher <yony252525@gmail.com>
2026-08-25 19:41:14 +0300

    fix

--- ftplugin/lean.lua
+++ ftplugin/lean.lua
@@ -1,9 +1,8 @@
-buf = args.buf
 -- This is a nicer comment string in my opinion.
-vim.bo[buf].commentstring = '-- %s'
+vim.bo.commentstring = '-- %s'
 -- Disable spell checking in Lean files, since it is not very useful.
 -- Spell checking is a window-local option, we can't set it per-buffer,
 -- so we just set it for the current window.
 vim.wo.spell = false
-vim.bo[buf].tw = 100
+vim.bo.tw = 100
 vim.wo.colorcolumn = '101'
